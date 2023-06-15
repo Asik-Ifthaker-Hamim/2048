fedce19dcd55c7806f8571182ecefb7f265e1480
Sayem <sayemahamed183@gmail.com>
2023-06-15 23:34:04 +0600

    added mergeSort

---
+++ mergeSort.js
@@ -0,0 +1,18 @@
+function mergeSort(array) {
+  if (array.length < 2) return array;
+  let mid = Math.floor(array.length / 2);
+  let leftArray = array.slice(0, mid);
+  let rightArray = array.slice(mid, array.length);
+  return merge(mergeSort(leftArray), mergeSort(rightArray));
+}
+function merge(leftArray, rightArray) {
+  let ans = [];
+  while (leftArray.length && rightArray.length) {
+    if (leftArray[0] < rightArray[0]) {
+      ans.push(leftArray.shift());
+    } else {
+      ans.push(rightArray.shift());
+    }
+  }
+  return [...ans, ...leftArray, ...rightArray];
+}
