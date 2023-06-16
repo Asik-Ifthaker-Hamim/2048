92038a148207243f7d4ba7ea08b58bb3a96315ce
A.M. Asik Ifthaker Hamim <109816005+Asik-Ifthaker-Hamim@users.noreply.github.com>
2023-06-16 20:41:03 +0600

    Update IScript.js

--- IScript.js
+++ IScript.js
@@ -1,8 +1,8 @@
 let draw = () => {
-  return (document.getElementById("board").innerHTML = data
-    .map((item) => {
-      return `<div class="player">${item.name}:${item.score}</div>`;
-    })
-    .join(""));
-};
-draw();
+    return (document.getElementById("board").innerHTML = data
+      .map((item) => {
+        return `<div class="player">${item.name}: ${item.score}</div>`;
+      })
+      .join(""));
+  };
+  draw();
